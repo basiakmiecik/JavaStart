311e0a6d92926bbb127fb6f8c6d3322712df1c0d
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-11 17:45:52 +0200

    dodano kalkulator

---
+++ zajecia02b/src/kalkulator.java
@@ -0,0 +1,15 @@
+public class kalkulator {
+    public static void main(String[] args){
+        double x=5.0;
+        int y=3;
+        double add=x+y;
+        double sub=x-y;
+        double diff=x/y;
+        double multi=x*y;
+        System.out.println(x+"+"+y+"="+add);
+        System.out.println(x+"-"+y+"="+sub);
+        System.out.println(x+"/"+y+"="+diff);
+        System.out.println(x+"*"+y+"="+multi);
+    }
+
+}
