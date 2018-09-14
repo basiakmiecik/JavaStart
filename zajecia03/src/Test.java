785d765b67cdd468e83198ee7ffbf866b1374466
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-14 17:33:11 +0200

    dodanie plikow Calculator Test itp

---
+++ zajecia03/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Calculator calc= new Calculator();
+        int suma =calc.add(6,5);
+        System.out.println(suma);
+
+        int suma2 = calc.add(2, 3);
+        System.out.println(suma2);
+    }
+}
